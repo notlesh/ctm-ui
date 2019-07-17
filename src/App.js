cf4c0cc4c35897c42e873af1501008131f4f29e0
Stephen Shelton <stephen@stephenashelton.com>
2019-07-17 12:04:40 -0600

    Initial checkin with stubbed navigation

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import MainAppDrawer from './MainAppDrawer';
+
+import './App.css';
+
+function App() {
+  return (
+    <MainAppDrawer />
+  );
+}
+
+export default App;
